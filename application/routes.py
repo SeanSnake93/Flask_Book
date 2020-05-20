2915b6f61c16e4f664f166beebf22203ad352bf4
SeanSnake93 <Sean.D.McCann@outlook.com>
2020-05-20 13:56:58 +0100

    update

--- application/routes.py
+++ application/routes.py
@@ -18,7 +18,7 @@ def catalogue():
     filmData = Films.query.all()
     return render_template('catalogue.html', title='catalogue Page', films=filmData)
 
-@app.route('/catalogue/<name>/add', methods=['GET','POST'])
+@app.route('/catalogue/<film>/add', methods=['GET','POST'])
 def add_collection(film):
     userID = current_user.id
     collectionData = Collection.query.all()
