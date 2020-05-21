9cbe9a89a784486972f208429a7766daeb06f515
SeanSnake93 <Sean.D.McCann@outlook.com>
2020-05-21 15:21:07 +0100

    jenkins

--- script/installation.sh
+++ script/installation.sh
@@ -9,13 +9,13 @@ sudo apt install python3-pip -y
 sudo apt install python3-venv -y
  
 python3 -m venv venv
- 
+
+source ~/.bashrc
+
 source /var/lib/jenkins/workspace/Flask_book/venv/bin/activate
  
 pip3 install -r requirements.txt
- 
+
 cd /var/lib/jenkins/workspace/Flask_book
- 
-source ~/.bashrc
- 
+
 gunicorn --bind=0.0.0.0:5000 app:app
