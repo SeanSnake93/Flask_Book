52e46ecabaaa29e8c23682bb9d6640e00b05a386
SeanSnake93 <Sean.D.McCann@outlook.com>
2020-05-22 14:27:39 +0100

    test

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -11,7 +11,7 @@ class TestBase(TestCase):
     def create_app(self):
         config_name = 'testing'
         app.config.update(
-            SQLALCHEMY_URI=getenv('FLASK_BOOK_TEST_URI'),
+            SQLALCHEMY_DATABASE_URI=getenv('FLASK_BOOK_TEST_URI'),
             SECRET_KEY=getenv('TEST_SECRET_KEY'),
             WTF_CSRF_ENABLED=False,
             DEBUG=True
