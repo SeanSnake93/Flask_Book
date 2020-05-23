7bbb9aa51fc08878d628eaacafa4414949f8366a
SeanSnake93 <Sean.D.McCann@outlook.com>
2020-05-23 18:07:45 +0100

    testing

--- tests/test_back_end.py
+++ tests/test_back_end.py
@@ -211,23 +211,6 @@ class TestOwnDuplicatesF(TestBase):
 
 # ---------- Read-Function-Testing ----------
 
-class TestReadFilmF(TestBase):
-    def test_edit_film(self):
-        """This is to Edit a film to the database 'Test Matrix 1011' in to 'Test Matrix 1111' with this test"""
-        with self.client:
-            self.client.post(
-                url_for('login'),
-                data=dict(
-                    email="AdminSystem@Testing.com",
-                    password="Adm1nSy5temT35t1n8"
-                ),
-            follow_redirects=True
-            )
-            response = self.client.post(
-                url_for('edit_movie', filmID = 2)
-            )
-        self.assertIn(b'Test Matrix 1011', title.data)
-
 # -------- END-Read-Function-Testing --------
 
 # ____________________________________________________________________
